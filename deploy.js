abb4d2980950b774e3365f0ee27d099e8c4a1fac
phongnhat19 <phongnhat19@gmail.com>
2020-11-04 16:20:53 +0700

    commit all

--- deploy.js
+++ deploy.js
@@ -1,25 +1,37 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
 const Web3 = require('web3')
-const compiledContract = require('./compile')
+const { Inbox, Lottery } = require('./compile')
 
 const provider = new HDWalletProvider(
     'nest volcano injury naive raw orange rich topple monitor divert future carpet',
-    'https://ropsten.infura.io/v3/48c040c780b34ed9acce325dffdc7faf'
+    'https://ropsten.infura.io/v3/1f03910a1fa3406dbf37a3e2c2fef867'
 )
 
 const web3 = new Web3(provider)
 
-const deploy = async () => {
+const deployInbox = async () => {
     const accounts = await web3.eth.getAccounts()
-    console.log(`Attempting to deploy from account: ${accounts[0]}`)
+    console.log(`Attempting to deploy Inbox from account: ${accounts[0]}`)
 
     const DEFAULT_MESSAGE = ''
 
-    const result = await new web3.eth.Contract(JSON.parse(compiledContract.interface))
-        .deploy({ data: compiledContract.bytecode, arguments: [DEFAULT_MESSAGE] })
+    const result = await new web3.eth.Contract(JSON.parse(Inbox.interface))
+        .deploy({ data: Inbox.bytecode, arguments: [DEFAULT_MESSAGE] })
         .send({ from: accounts[0], gas: '1000000' })
     
-    console.log(`SMC deployed to: ${result.options.address}`)
+    console.log(`Inbox SMC deployed to: ${result.options.address}`)
 };
 
-deploy();
+const deployLottery = async () => {
+    const accounts = await web3.eth.getAccounts()
+    console.log(`Attempting to deploy Lottery from account: ${accounts[0]}`)
+
+    const result = await new web3.eth.Contract(JSON.parse(Lottery.interface))
+        .deploy({ data: Lottery.bytecode })
+        .send({ from: accounts[0], gas: '1000000' })
+    
+    console.log(`Lottery SMC deployed to: ${result.options.address}`)
+};
+
+deployInbox();
+deployLottery();
--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,10 +1,37 @@
 const Web3 = require('web3')
-const compiledContract = require('../compile')
+const compiledContract = require('../compile').Lottery
 const ganache = require('ganache-cli')
 
 const provider = ganache.provider()
 const web3 = new Web3(provider)
 
 describe('Lottery', () => {
-    
+    let accounts, lottery;
+    beforeEach(async () => {
+        accounts = await web3.eth.getAccounts()
+
+        lottery = await new web3.eth.Contract(JSON.parse(compiledContract.interface))
+            .deploy({ data: compiledContract.bytecode })
+            .send({ from: accounts[0], gas: '1000000' })
+
+        lottery.setProvider(provider)
+    })
+
+    it('deploy a contract', () => {
+        expect(lottery.options.address).toBeTruthy();
+    })
+
+    it('allow account to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('.02', 'ether')
+        })
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        })
+        expect(Array.isArray(players)).toEqual(true)
+        expect(players.length).toEqual(1)
+        expect(players[0]).toEqual(accounts[0])
+    })
 })
